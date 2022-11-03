2c48e637dd7bbaae4496d80b4d7dbf8b0b8361e8
noguchitakuya <takuya.noguchi@rakus-partners.co.jp>
2022-11-03 16:03:24 +0900

    humbergericonの動きが完成

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,10 +1,12 @@
 import "../scss/style.scss";
 
 const humberger_icon = document.querySelector(".humberger_icon");
+const humberger_icon_line = document.querySelector(".humberger_icon_line");
 const contents_add = document.querySelector(".contents_add");
 const contents_add_inner = document.querySelector(".contents_add_inner");
 
 humberger_icon.addEventListener("click", () => {
   contents_add.classList.toggle("contents_add_active");
   contents_add_inner.classList.toggle("contents_add_inner_active");
+  humberger_icon_line.classList.toggle("humberger_icon_line_active");
 });
