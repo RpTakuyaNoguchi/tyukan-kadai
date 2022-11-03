ad99ab47d9c0aa61b3e8aaef72f96996a052e4f4
noguchitakuya <takuya.noguchi@rakus-partners.co.jp>
2022-11-03 15:18:51 +0900

    contents_addをスクロールする機能が完成

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,1 +1,10 @@
 import "../scss/style.scss";
+
+const humberger_icon = document.querySelector(".humberger_icon");
+const contents_add = document.querySelector(".contents_add");
+const contents_add_inner = document.querySelector(".contents_add_inner");
+
+humberger_icon.addEventListener("click", () => {
+  contents_add.classList.toggle("contents_add_active");
+  contents_add_inner.classList.toggle("contents_add_inner_active");
+});
