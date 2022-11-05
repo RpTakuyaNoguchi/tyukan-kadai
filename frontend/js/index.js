e20fde523bcdd82dae2804e6ddbb32489b4e32ae
RpTakuyaNoguchi <ta.noguchi@opt-incubate.com>
2022-11-05 21:53:31 +0900

    ハンバーガーアイコンアニメーション完成

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -4,9 +4,11 @@ const humberger_icon = document.querySelector(".humberger_icon");
 const humberger_icon_line = document.querySelector(".humberger_icon_line");
 const contents_add = document.querySelector(".contents_add");
 const contents_add_buttons = document.querySelector(".contents_add_buttons");
+const contents_list = document.querySelector(".contents_list");
 
 humberger_icon.addEventListener("click", () => {
+  humberger_icon_line.classList.toggle("is_active");
   contents_add.classList.toggle("is_active");
-  humberger_icon_line.classList.toggle("humberger_icon_line_active");
   contents_add_buttons.classList.toggle("is_active");
+  contents_list.classList.toggle("is_active");
 });
