d3bdeb27de5b3bef20aea1f913351587246693ce
RpTakuyaNoguchi <ta.noguchi@opt-incubate.com>
2022-11-05 23:10:59 +0900

    basicファイルの作成、color変数の作成

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,6 +1,7 @@
 import "../scss/style.scss";
 
 const humberger_icon = document.querySelector(".humberger_icon");
+const group_add_icon = document.querySelector(".group_add_icon");
 const humberger_icon_line = document.querySelector(".humberger_icon_line");
 const contents_add = document.querySelector(".contents_add");
 const contents_add_buttons = document.querySelector(".contents_add_buttons");
@@ -8,6 +9,10 @@ const contents_list = document.querySelector(".contents_list");
 
 humberger_icon.addEventListener("click", () => {
   humberger_icon_line.classList.toggle("is_active");
+});
+
+group_add_icon.addEventListener("click", () => {
+  group_add_icon.classList.toggle("is_active");
   contents_add.classList.toggle("is_active");
   contents_add_buttons.classList.toggle("is_active");
   contents_list.classList.toggle("is_active");
