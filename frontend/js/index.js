600167c47551c05f9b905e1c28008b4d19033f5f
RpTakuyaNoguchi <ta.noguchi@opt-incubate.com>
2022-11-10 00:47:49 +0900

    スマホ版追加シート完成

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,13 +1,10 @@
 import "../scss/style.scss";
 
 const humberger_icon = document.querySelector(".humberger_icon");
-
 const side_bar_contents_icon = document.querySelector(
     ".side_bar_contents_icon"
 );
-
 const js_contents_icon = document.querySelector(".js_contents_icon");
-
 const js_add_sheet_open = document.querySelector(".js_add_sheet_open");
 
 const humberger_icon_line = document.querySelector(".humberger_icon_line");
@@ -15,26 +12,35 @@ const contents_add = document.querySelector(".contents_add");
 const contents_add_buttons = document.querySelector(".contents_add_buttons");
 const contents_list = document.querySelector(".contents_list");
 
+// ハンバーガーアイコンをクリックしたときのイベント
 humberger_icon.addEventListener("click", () => {
     humberger_icon_line.classList.toggle("is_active");
 });
 
-side_bar_contents_icon.addEventListener("click", () => {
+// 追加シートを開閉するときのファンクション
+function sheet_is_active() {
+    const is_active = contents_add.classList.toggle("is_active");
     side_bar_contents_icon.classList.toggle("is_active");
-    contents_add.classList.toggle("is_active");
     contents_add_buttons.classList.toggle("is_active");
     contents_list.classList.toggle("is_active");
-});
-
-js_contents_icon.addEventListener("click", () => {
-    js_contents_icon.classList.toggle("is_active");
-});
-
-js_add_sheet_open.addEventListener("click", () => {
-    const is_active = contents_add.classList.toggle("is_active");
     if (is_active === true) {
         document.getElementsByTagName("body")[0].style.overflow = "hidden";
     } else {
         document.getElementsByTagName("body")[0].style.overflow = "visible";
     }
+}
+
+// スマホでコンテンツアイコンをクリックしたときのイベント
+js_contents_icon.addEventListener("click", () => {
+    js_contents_icon.classList.toggle("is_active");
+});
+
+// サイドバーのコンテンツアイコンをクリックしたときのイベント
+side_bar_contents_icon.addEventListener("click", () => {
+    sheet_is_active();
+});
+
+// スマホの追加ボタンをクリックしたときのイベント
+js_add_sheet_open.addEventListener("click", () => {
+    sheet_is_active();
 });
