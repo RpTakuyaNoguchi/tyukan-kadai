66cee42ba099aea7e494cf326441b95844d857d0
RpTakuyaNoguchi <ta.noguchi@opt-incubate.com>
2022-11-09 12:45:34 +0900

    ss

--- frontend/npm/webpack.config.js
+++ frontend/npm/webpack.config.js
@@ -1,59 +1,59 @@
 // プラグインを利用するためにwebpackを読み込んでおく
-const webpack = require('webpack');
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require("webpack");
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 //サービス全体で利用
 const main = {
-    // メインとなるJavaScriptファイル（エントリーポイント）
-    entry: "../js/index.js",
-    mode: "production",
-    // mode: "development",
-    resolve: {
-      modules: [path.resolve(__dirname, '../npm/node_modules')],
-      alias: {
-        '@pickadate': 'pickadate/lib/'
-      }
+  // メインとなるJavaScriptファイル（エントリーポイント）
+  entry: "../js/index.js",
+  // mode: "production",
+  mode: "development",
+  resolve: {
+    modules: [path.resolve(__dirname, "../npm/node_modules")],
+    alias: {
+      "@pickadate": "pickadate/lib/",
     },
-    // ファイルの出力設定
-    output: {
-        //  出力ファイルのディレクトリ名
-        path: path.resolve(__dirname, '../../static/webpack'),
-        // 出力ファイル名
-        filename: "bundle.js"
-        // hash値自動付与 変更時はwebpack再起動
-        // filename: "bundle_[hash].js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss/,
-                //ローダーの処理対象となるディレクトリ
-                include: path.resolve(__dirname, '../scss'),
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                    },
-                    "css-loader",
-                    "sass-loader",
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    { loader: MiniCssExtractPlugin.loader },
-                    { loader: 'css-loader' }
-                ]
-            }
-        ]
-    },
-    plugins: [
-        // 外部CSSファイルを出力するプラグイン
-        new MiniCssExtractPlugin({
-            path: path.resolve(__dirname, '../../static/webpack'),
-            filename: 'style.css',
-        }),
+  },
+  // ファイルの出力設定
+  output: {
+    //  出力ファイルのディレクトリ名
+    path: path.resolve(__dirname, "../../static/webpack"),
+    // 出力ファイル名
+    filename: "bundle.js",
+    // hash値自動付与 変更時はwebpack再起動
+    // filename: "bundle_[hash].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss/,
+        //ローダーの処理対象となるディレクトリ
+        include: path.resolve(__dirname, "../scss"),
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          { loader: "css-loader" },
+        ],
+      },
     ],
+  },
+  plugins: [
+    // 外部CSSファイルを出力するプラグイン
+    new MiniCssExtractPlugin({
+      path: path.resolve(__dirname, "../../static/webpack"),
+      filename: "style.css",
+    }),
+  ],
 };
 
 /*
@@ -116,6 +116,4 @@ const service = {
 //     main, service
 // ];
 
-module.exports = [
-    main
-];
+module.exports = [main];
